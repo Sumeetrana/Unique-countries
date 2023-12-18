071ef006c04fce77942ad2ecc21eb4d56f8d5d44
sumeet.rana <sumeetsinh28@gmail.com>
2023-12-18 11:11:37 +0530

    feat: code complete

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+from countries_worked import main
+
+# Opening the excel file
+file = open('countries_confirmed.csv')
+
+# Reading the excel file
+csv_reader = csv.reader(file)
+countries_confirmed = []
+
+# Iterating through rows and 
+# appending them in countries_confirmed list
+for row in csv_reader:
+  if row:
+    countries_confirmed.append(row[0])
+
+# Reading values from another module
+countries_worked = main()
+
+# Finding out countries which are in countries_worked, but not in countries_confirmed
+unique_countries = set(countries_worked) - set(countries_confirmed)
+
+# Sorting and printing the data
+print(sorted(unique_countries))
